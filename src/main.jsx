22683c06b2a3f51f8368b93e30a580805fd4fd06
Mujeeb-Siddique <mujeebsiddique9@gmail.com>
2025-11-17 00:24:30 +0500

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import store from './Store/store.js'
 import './index.css'
 import App from './App.jsx'
 
+
 createRoot(document.getElementById('root')).render(
-    
-      <StrictMode>
-        <Provider store={store}>
+  <StrictMode>
+    <Provider store={store}>
+      
         <App />
-      </Provider>
-      </StrictMode>
+      
+    </Provider>
+  </StrictMode>
 )
